c62a59c56f00e679f185fc2718f8daaa85d36117
Dids Irwyn Reyes <dids.irwyn@gmail.com>
2024-04-07 13:14:55 +0800

    Added Footer & FAB

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import {LiaUserSecretSolid} from 'react-icons/lia';
 import {TbGhost2} from 'react-icons/tb';
+import Lottie from 'react-lottie';
+import animationData from '../lotties/ghost.json';
 
 const truncateMessage = (message, maxLength) => {
   const secondSpaceIndex = message.indexOf(' ', message.indexOf(' ') + 1);
@@ -18,6 +20,22 @@ function Letter({letter, toggleDetailsModal, setSelectedLetter}) {
     setSelectedLetter(letter);
     toggleDetailsModal();
   };
+
+  if (letter == null) {
+    return (
+      <div>
+        <Lottie
+          options={{
+            animationData: animationData,
+            loop: true,
+            autoplay: true,
+          }}
+          height={200}
+          width={200}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="letter-card" onClick={handleClick}>
