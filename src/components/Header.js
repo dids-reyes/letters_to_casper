cfaf133f7c7b4f07e63b667b3c7ee35d94e68808
Dids Irwyn Reyes <dids.irwyn@gmail.com>
2024-04-10 17:38:32 +0800

    Added typewriter animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,66 @@
-// Header.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from '../lotties/ltc_logo_1.webp';
 import {Tooltip} from 'react-tooltip';
+import Typewriter from 'typewriter-effect';
 
 function Header({searchTerm, handleSearchChange}) {
+  const [showLogo, setShowLogo] = useState(true);
+  const [alternateMessage, setAlternateMessage] = useState('');
+
+  // Array of alternate messages
+  const letters = [
+    'Unsent messages are the ghosts of conversations that haunt us.',
+    // 'Every unsent message holds a story left untold.',
+    // 'Behind every unsent message lies an untold emotion.',
+    // "I have so much I want to say to you but I can't. All I can do is write it down and never send it - Sylvia Plath",
+    // 'Ang Pag-iibigan natin ay muling maisusulat sa Huling pagkakataon... at ito ang Kahilingan ko. - Carmela Isabella',
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setShowLogo(prevShowLogo => !prevShowLogo);
+    }, 10000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Run effect only once on component mount
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Display a random alternate message
+      const randomIndex = Math.floor(Math.random() * letters.length);
+      setAlternateMessage(letters[randomIndex]);
+    }, 5000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [letters]); // Run effect when letters array changes
+
   return (
     <div className="header">
       <br />
-      <img
-        className="logo"
-        src={logo}
-        alt="Letters to Casper"
-        width="400"
-        height="70"
-      />
+      {showLogo ? (
+        <img
+          className="logo"
+          src={logo}
+          alt="Letters to Casper"
+          width="400"
+          height="70"
+        />
+      ) : (
+        <p className="alt-logo">
+          <Typewriter
+            options={{delay: 20, loop: false}}
+            onInit={typewriter => {
+              typewriter
+                .typeString(alternateMessage)
+                .pauseFor(3000)
+                .deleteAll(1)
+                .start();
+            }}
+          />
+        </p>
+      )}
 
       <div className="search-bar">
         <a
@@ -32,7 +79,6 @@ function Header({searchTerm, handleSearchChange}) {
             placeholder="🔍 Search Letters"
           />
         </a>
-        <Tooltip id="my-tooltip" />
       </div>
     </div>
   );
