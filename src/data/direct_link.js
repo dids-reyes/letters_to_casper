6c1af17846649753d0b4fc8cf64499bd918ba1ea
Dids Irwyn Reyes <dids.irwyn@gmail.com>
2024-06-17 19:15:24 +0800

    Replace ad url to anti-adblock

--- src/data/direct_link.js
+++ src/data/direct_link.js
@@ -1,3 +1,3 @@
 export function displayDirectLinkAds(){
-  window.open('https://www.highrevenuenetwork.com/h65p1hjab?key=0f5e28f15ad6525e5be830e529cabd5e', '_blank');
+  window.open('https://twoepidemic.com/h65p1hjab?key=0f5e28f15ad6525e5be830e529cabd5e', '_blank');
 }
