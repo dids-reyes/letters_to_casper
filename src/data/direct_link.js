277a5673a030dbcb65d945cdb85574cb0ea335d0
Dids Irwyn Reyes <48916955+dids-reyes@users.noreply.github.com>
2025-07-31 01:19:56 +0800

    Update Direct Link

--- src/data/direct_link.js
+++ src/data/direct_link.js
@@ -1,3 +1,3 @@
 export function displayDirectLinkAds(){
-  window.open('https://twoepidemic.com/h65p1hjab?key=0f5e28f15ad6525e5be830e529cabd5e', '_blank');
+  window.open('https://www.profitableratecpm.com/xzexfiq5y?key=7d8e1dcd24f1a46828272926ff4b3891', '_blank');
 }
