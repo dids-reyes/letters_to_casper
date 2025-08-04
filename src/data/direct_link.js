82c6a6161d72abec0bfc75abd403a1a1ac969bad
Dids Irwyn Reyes <48916955+dids-reyes@users.noreply.github.com>
2025-08-04 23:40:47 +0800

    Anti-Adblock

--- src/data/direct_link.js
+++ src/data/direct_link.js
@@ -1,3 +1,3 @@
 export function displayDirectLinkAds(){
-  window.open('https://www.profitableratecpm.com/xzexfiq5y?key=7d8e1dcd24f1a46828272926ff4b3891', '_blank');
+  window.open('https://storyunicornupper.com/xzexfiq5y?key=7d8e1dcd24f1a46828272926ff4b3891', '_blank');
 }
