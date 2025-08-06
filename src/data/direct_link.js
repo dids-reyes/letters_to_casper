39ce9c08b4343a6f02923356504ab176baae09ff
Dids Irwyn Reyes <48916955+dids-reyes@users.noreply.github.com>
2025-08-06 20:28:24 +0800

    Update Adsterra

--- src/data/direct_link.js
+++ src/data/direct_link.js
@@ -1,3 +1,3 @@
 export function displayDirectLinkAds(){
-  window.open('https://storyunicornupper.com/xzexfiq5y?key=7d8e1dcd24f1a46828272926ff4b3891', '_blank');
+  window.open('https://storyunicornupper.com/rxyce75in3?key=945fab619a2a948227fecaaf9d93f787', '_blank');
 }
