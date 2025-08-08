c66ea409fd7d8e5d0f7c0567e3fdaf27968ecf8d
Dids Irwyn Reyes <dids.irwyn@gmail.com>
2025-08-09 02:49:37 +0800

    Revert

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>
-          If you want to request deletion of your post, contact us at{' '}
-          letters2casper@gmail.com{' '}
+          If you want to request deletion of your post, contact us at{" "}
+          letters2casper@gmail.com{" "}
         </p>
         <p>
-          <Link to="/seek_help" style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>Seek Help</Link> |&nbsp;
-          <Link to="/privacy_policy" style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>Privacy Policy</Link> |&nbsp;
-          <Link to="/terms_and_conditions" style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>Terms & Conditions</Link> |&nbsp;
-          <span style={{ color: 'white', fontWeight: 'bold' }}>Developers</span> |&nbsp;
-          <Link to="/about_us" style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>About Us</Link>
+          <Link
+            to="/seek_help"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            Seek Help
+          </Link>{" "}
+          |&nbsp;
+          <Link
+            to="/privacy_policy"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            Privacy Policy
+          </Link>{" "}
+          |&nbsp;
+          <Link
+            to="/terms_and_conditions"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            Terms & Conditions
+          </Link>{" "}
+          |&nbsp;
+          <Link
+            to="/developer_portal"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            Developers
+          </Link>{" "}
+          |&nbsp;
+          <Link
+            to="/about_us"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            About Us
+          </Link>
         </p>
       </div>
     </footer>
